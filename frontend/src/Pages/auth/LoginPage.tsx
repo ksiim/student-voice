03e960db0e46ebc83a50b547100ab0235407c466
mambbas <arzontum@gmail.com>
2024-10-27 17:31:48 +0500

    Master (#6)
    * models like in template
    
    * added Role, Class models
    
    * added Subject model
    
    * added Room, Building models
    
    * added RoomPublic and RoomsPublic classes
    
    * added
    Attendance model
    
    * added Review model
    
    * added back relationships
    
    * added some structure to project
    made some layout to pages
    
    * added some structure and started making pages layout
    
    * test commi
    
    ---------
    
    Co-authored-by: ksiim <makskrutchev@gmail.com>
    Co-authored-by: LicoriceAlex <aleksandrsavin03@mail.ru>

---
+++ frontend/src/Pages/auth/LoginPage.tsx
@@ -0,0 +1,15 @@
+import {Logo} from '../../Components/Logo.tsx'
+import {LoginForm} from './Components/LoginForm.tsx';
+
+function LoginPage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <div className="w-full max-w-md">
+        <Logo />
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
