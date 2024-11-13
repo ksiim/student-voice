58df4cd04a9b8847c8ca625024ddc9224d17a409
ksiim <makskrutchev@gmail.com>
2024-11-13 21:33:45 +0500

    added todo for frontend

--- backend/app/api/routes/classes.py
+++ backend/app/api/routes/classes.py
@@ -1,7 +1,9 @@
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
+import uuid
 from app import crud
 from app.models import Class, ClassCreate, ClassPublic, ClassesPublic
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 
 from app.api.deps import (
     SessionDep,
@@ -28,13 +30,33 @@ async def create_class(*, session: SessionDep, class_in: ClassCreate) -> Any:
     # dependencies=[Depends(get_current_active_superuser)],
     response_model=ClassesPublic
 )
-async def read_classes(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
+async def read_classes(
+    session: SessionDep,
+    skip: int = 0,
+    limit: int = 100,
+    start_time: Optional[datetime] = Query(None),
+    end_time: Optional[datetime] = Query(None),
+    room_id: Optional[uuid.UUID] = Query(None),
+    teacher_id: Optional[uuid.UUID] = Query(None)
+) -> Any:
     """
     Retrieve classes.
     """
-    count_statement = select(func.count()).select_from(Class)
+    statement = select(Class)
+    
+    if start_time:
+        statement = statement.where(Class.start_time >= start_time)
+    if end_time:
+        statement = statement.where(Class.end_time <= end_time)
+    if room_id:
+        statement = statement.where(Class.room_id == room_id)
+    if teacher_id:
+        statement = statement.where(Class.teacher_id == teacher_id)
+    
+    count_statement = select(func.count()).select_from(statement.subquery())
     count = (await session.execute(count_statement)).scalar()
-    statement = select(Class).offset(skip).limit(limit)
-    users = (await session.execute(statement)).scalars().all()
+    
+    statement = statement.offset(skip).limit(limit)
+    classes = (await session.execute(statement)).scalars().all()
 
-    return ClassesPublic(data=users, count=count)
+    return ClassesPublic(data=classes, count=count)
